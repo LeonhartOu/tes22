6691f3da1ad0233b9e299b00b9b648027528f67e
Ricky Krisdianto <90753454+LeonhartOu@users.noreply.github.com>
2025-02-28 13:21:32 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Hashmicro Test</title>
+    <title>Test</title>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css" rel="stylesheet">
